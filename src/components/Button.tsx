8c8220dfc8a565da173ab1fab55e33a552599bd9
rafinhabusatta <lacerdabusatta@gmail.com>
2022-10-14 10:21:55 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,22 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+/* necessário passar um children na interface
+   pois Button precisa de algo nele */
+export interface ButtonProps {
+  children: ReactNode;
+  asChild?: boolean;
+}
+
+export function Button({ children, asChild }: ButtonProps) {
+  const Comp = asChild ? Slot : 'button';
+  return (
+    <Comp 
+    className={clsx(
+      'px-4 py-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+    )}
+    >
+      {children}
+    </Comp>
+  )
+}
