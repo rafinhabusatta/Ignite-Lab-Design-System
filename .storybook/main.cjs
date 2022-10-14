a4110f3c624e12dc2ef7679eeb78d580677f47cf
rafinhabusatta <lacerdabusatta@gmail.com>
2022-10-14 13:09:36 -0300

    modificações nos componentes; criação da interface com os componentes

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -3,7 +3,8 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@stoorybook/addon-a11y'
   ],
   framework: '@storybook/react',
   core: {
